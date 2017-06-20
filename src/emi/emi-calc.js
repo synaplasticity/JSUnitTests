1349b6e66ab71de6c2a41cf51b1c408eb3ac7271
synaplasticity <synaplasticity@gmail.com>
2017-06-20 15:43:19 +0530

    More tests

--- src/emi/emi-calc.js
+++ src/emi/emi-calc.js
@@ -5,6 +5,26 @@ function EmiCalc() {
 }
 
     EmiCalc.prototype.getEmi = function(principal, rate, duration) {
+
+        if (principal === undefined || principal === NaN) {
+            throw Error('Principal cannot be empty.');
+        }
+        if (rate === undefined || rate === NaN) {
+            throw Error('Rate cannot be empty.');
+        }
+        if (duration === undefined || duration === NaN) {
+            throw Error('Duration cannot be empty.');
+        }
+        if (Math.sign(principal) === -1 || Math.sign(rate) === -1 || Math.sign(duration) === -1) {
+            throw Error("Values cannot be negative.");
+        }
+        if (duration > Number.MAX_SAFE_INTEGER) {
+            throw Error("Duration cannot be larger than Number.MAX_SAFE_INTEGER.");
+        }
+        if (duration <= 0) {
+            throw Error("Duration cannot be zero or negative.");
+        }
+
         var emi = ( principal * rate * (Math.pow(rate+1, duration)) )
                             / Math.pow(rate+1, duration)-1;
 
--- tests/emi/emi_tests.js
+++ tests/emi/emi_tests.js
@@ -8,12 +8,74 @@ var emi = require('../../src/emi/emi-calc');
 describe("EMI tests", function() {
     var emiCalc = new emi();
 
-    it ('Should get 24999 as the emi value', function() {
-
+    it ('Should get 24999.00 as the emi value', function() {
         var emiValue = emiCalc.getEmi(500000, 0.05, 120);
-
         expect(emiValue).to.equal('24999.00');
 
-    })
+    });
+
+    it ('Should throw an exception when principal is empty', function() {
+        expect(function() {
+            var emiValue = emiCalc.getEmi(undefined, 0.05, 120);
+        }).to.throw(Error);
+
+    });
+
+    it ('Should throw an exception when rate is empty', function() {
+        expect(function() {
+            var emiValue = emiCalc.getEmi(500000, undefined, 120);
+        }).to.throw(Error);
+
+    });
+
+    it ('Should throw an exception when duration is empty', function() {
+        expect(function() {
+            var emiValue = emiCalc.getEmi(500000, 0.05, undefined);
+        }).to.throw(Error);
+
+    });
+
+    it ('Should throw an exception when principal is negative', function() {
+        expect(function() {
+            var emiValue = emiCalc.getEmi(-500000, 0.05, 120);
+        }).to.throw(Error);
+
+    });
+
+    it ('Should throw an exception when rate is negative', function() {
+        expect(function() {
+            var emiValue = emiCalc.getEmi(500000, -0.05, 120);
+        }).to.throw(Error);
+
+    });
+
+    it ('Should throw an exception when duration is negative', function() {
+        expect(function() {
+            var emiValue = emiCalc.getEmi(500000, 0.05, -120);
+        }).to.throw(Error);
+
+    });
+
+    it ('Should throw an exception when duration is a large unsafe number', function() {
+        expect(function() {
+            var emiValue = emiCalc.getEmi(5000, 0.01, Number.MAX_VALUE);
+        }).to.throw(Error);
+
+    });
+
+    it ('Should throw an exception when duration is a less than equal to 0', function() {
+        expect(function() {
+            var emiValue = emiCalc.getEmi(10000, 0.05, -1);
+        }).to.throw(Error);
+
+    });
+
+    it ('Should return Infinity for large unsafe inputs for duration and rate', function() {
+
+        var emiValue = emiCalc.getEmi(Number.MAX_VALUE, Number.MAX_VALUE, 1);
+        expect(emiValue).to.equal('Infinity');
+
+    });
+
 
 });
